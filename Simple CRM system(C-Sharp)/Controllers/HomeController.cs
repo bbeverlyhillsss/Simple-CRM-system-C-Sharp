abdac2f04d26da3e03a2eff6bd7a441678502fab
bbeverlyhillsss <bbeverlyhillsss@gmail.com>
2026-03-08 17:11:59 +0000

    Add project files.

---
+++ Simple CRM system(C-Sharp)/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Simple_CRM_system_C_Sharp_.Models;
+using System.Diagnostics;
+
+namespace Simple_CRM_system_C_Sharp_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Simple CRM system(C-Sharp)/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Simple_CRM_system_C_Sharp_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
